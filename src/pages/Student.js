e07f6cf60ba1b4d8d0b8b8908acb0bcf30e84610
Electrifire-123 <rahullyadavv77@gmail.com>
2022-05-24 21:15:53 +0530

    inserted table in student

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Student = () => {
   return (
-    <div className='student_page'>
+    <div className="container text-center">
       <h1>Student</h1>
+      <table class="table table-bordered border-primary">
+        <thead>
+          <tr>
+            <th scope="col"> Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Course</th>
+            <th scope="col">Batch</th>
+            <th scope="col">change</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th >Rahul</th>
+            <td>21</td>
+            <td>MERN</td>
+            <td>October</td>
+            <td>Edit</td>
+          </tr>
+          <tr>
+            <th >Sarang</th>
+            <td>20</td>
+            <td>MERN</td>
+            <td>November</td>
+            <td>Edit</td>
+          </tr>
+          <tr>
+            <th >Harsh</th>
+            <td>19</td>
+            <td>MERN</td>
+            <td>November
+            </td>
+            <td>Edit</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Student
+export default Student;
